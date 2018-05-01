3a4f7a133a5abc41059f702b7789706074f0e82a
user <user@PC>
2018-05-02 00:53:23 +0600

    Add project files.

---
+++ ServerApp_For_Chat/Modules/Chat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace ServerApp_For_Chat.Modules
+{
+    public sealed class Chat : NancyModule
+    {
+        public Chat()
+        {
+            Get("/GetLocalhostWithPort", a => "http://localhost:52312/");
+            Post("/AddMessage/{GroupName}/{Message}", a =>
+            {
+                try
+                {
+                    DirectoryInfo dir = new DirectoryInfo(AppContext.BaseDirectory).Parent?.Parent?.Parent;
+                    DirectoryInfo info = Directory.CreateDirectory(dir?.FullName + $"/{a.GroupName}");
+                    string fullpath = $"{info.FullName}/{a.GroupName}.txt";
+                    if (!File.Exists(fullpath))
+                    {
+                        FileStream chatFileStream = File.Create(fullpath);
+                        chatFileStream.Dispose();
+                    }
+                    using (StreamWriter sw = new StreamWriter(fullpath, true))
+                    {
+                        sw.WriteLine(String.Format("{0}", a.Message));
+                    }
+                    return "Ok";
+                }
+                catch (Exception e)
+                {
+                    return e.Message;
+                }
+            });
+        }
+    }
+}
---
+++ ServerApp_For_Chat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ServerApp_For_Chat
+{
+    public class Program
+    {
+        public static string Ip = "localhost", Login = "best", Password = "liza1999", VirtualHost = "/";
+
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
+        }
+    }
+}
